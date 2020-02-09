70254faffbe50b0e15bdd7d35a8e159b9533dc76
explorer-c137 <60836798+explorer-c137@users.noreply.github.com>
2020-02-09 11:48:59 +0530

    Add files via upload

---
+++ basic calculator.py
@@ -0,0 +1,12 @@
+
+num1 = input("Enter 1st no: ")
+num2 = input("Enter 2nd no: ")
+#this wont get u ur answer, as ide recognizes it as string
+
+result2 = float(num1) + float(num2)
+print(result2)
+
+#now to use integer, int(num1) 
+#always use float() as int() is quite restricted
+
+
