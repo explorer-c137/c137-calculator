b56d4bd7ccda627b838bb49fd371d4bdaac1c85d
explorer-c137 <60836798+explorer-c137@users.noreply.github.com>
2020-03-17 19:49:44 +0530

    Add files via upload

---
+++ mad libs game.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a color:")
+noun = input("Enter a noun: ")
+celebrity_name = input("Enter a celebrity's name: ")
+
+print("roses are " + color)
+print(noun +" are blue")
+print("I love " + celebrity_name)
