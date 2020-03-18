4d9a7cfa693c1ad9bbc269c01e262625da7370b8
explorer-c137 <60836798+explorer-c137@users.noreply.github.com>
2020-03-18 14:07:26 +0530

    Add files via upload

---
+++ bhbh.py
@@ -0,0 +1,67 @@
+
+
+
+def cube(n):
+	print(n*n*n)
+
+cube(21)
+
+
+
+def square(num):
+	return num * num 
+
+print(square(4))
+
+
+
+
+
+
+
+def q(a ,b, c):
+	if a > b and a> c :
+		print(int(a))
+	elif a == b or a==c:
+		print('two values are similar, error')
+	elif b>a and b>c:
+		print(int(b))
+	elif a == b or b==c:
+		print('two values are similar, error')
+	elif c>a and c>b:
+		print(int(c))
+	elif c == b or a==c:
+		print('two values are similar, error')
+	else:
+		print("error")
+
+
+q(21738723, 121223, 912383)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ interesting functions.py
@@ -0,0 +1,62 @@
+
+
+
+def cube(n):
+	print(n*n*n)
+
+cube(21)
+
+def square(num):
+	return num * num 
+
+print(square(57))
+
+def q(a ,b, c) :
+	if a > b and a> c :
+		print(float(a))
+	elif a == b or a==c :
+		print('values are similar, error')
+	elif b > a and b > c :
+		print(float(b))
+	elif a == b or b==c :
+		print('values are similar, error')
+	elif c > a and c > b :
+		print(float(c))
+	elif c == b or a==c :
+		print('values are similar, error')
+	else :
+		print("error")
+		
+
+
+
+
+q(1212.23 , 12.1223 , 121223)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
